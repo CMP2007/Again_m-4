1d4a4af943f79b74ca1b1b7202da0b6fde30dccd
PucheCarlos <puchecarlos00@gmail.com>
2026-01-04 20:34:29 -0400

    ejercicio 4.6

--- ejercicio_4.1/tests/dummy.test.js
+++ ejercicio_4.1/tests/dummy.test.js
@@ -1,7 +1,19 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const { blogs } = require('./dataPrueba')
 const testsBlogs = require('./dataPrueba').blogs
+
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    __v: 0
+  }
+]
 
 test('dummy returns one', () => {
   const blogs = []
@@ -11,17 +23,6 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     assert.strictEqual(result, 5)
@@ -39,8 +40,7 @@ describe('total likes', () => {
 
 
 describe('More likes', ()=>{
-
-  test('la funcion haya el objeto con mas likes', ()=>{
+  test('la funcion encuentra el objeto con mas likes', ()=>{
     const result = listHelper.favoriteBlog(testsBlogs)
     const newObject = {
       title: testsBlogs[2].title,
@@ -55,25 +55,27 @@ describe('More likes', ()=>{
   })
 
   test('responde con un unico dato enviado', ()=>{
-    const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 5,
-      __v: 0
-    }
-  ]
   const responseOneBlog = {
         title: 'Go To Statement Considered Harmful',
         author: 'Edsger W. Dijkstra',
         likes: 5,
       }
-    console.log(listWithOneBlog);
-    const result = listHelper.favoriteBlog(listWithOneBlog)
-    console.log(result);
-    
+    const result = listHelper.favoriteBlog(listWithOneBlog)    
     assert.deepStrictEqual(result, responseOneBlog)
+  })
+})
+
+
+describe('More Blogs for author', ()=>{
+  test('la funcion encuentra al autor con mas blogs',()=>{
+    assert.deepStrictEqual(listHelper.mostBlogs(testsBlogs),{ author: 'Robert C. Martin', blogs: 3 })
+  })
+
+  test('la funcion maneja bien valores vacios', ()=>{
+    assert.strictEqual(listHelper.mostBlogs([]), null)
+  })
+
+  test('la funcion maneja un unico dato', ()=>{
+    assert.deepStrictEqual(listHelper.mostBlogs(listWithOneBlog), {author: 'Edsger W. Dijkstra', blogs:1})
   })
 })
