02bed28073dabb769594c8070c652492005e1581
PucheCarlos <puchecarlos00@gmail.com>
2026-01-20 23:43:13 -0400

    ejercio 4.11

--- ejercicio_4.1/tests/ejercise.test.js
+++ ejercicio_4.1/tests/ejercise.test.js
@@ -35,7 +35,7 @@ beforeEach(async () => {
 
 
 
-describe('controlladores tests', ()=>{
+describe('pruebas del controlador .get', ()=>{
     test('el codigo obtienes los datos de la base de Datos', async ()=>{
      const response = await api
         .get('/api/blogs')
@@ -45,30 +45,47 @@ describe('controlladores tests', ()=>{
          assert.strictEqual(response.body.length, 2)
     })
 
-    test('los datos poseen valor ID', async ()=>{
+    test('los datos poseen valor id y no el _id', async ()=>{
       const response = await api.get('/api/blogs')
       assert.ok(response.body[0].id)
       assert.strictEqual('_id' in response.body[0], false)
     })
+})
 
-    test('los datos son enviados con exito de forma correcta', async ()=>{
-      const newObjet = {
-    "title": "bbbb",
-    "author": "miguel",
-    "url": "www/hola.net",
-    "likes": 10,
+describe('pruebas del controlador .POST',()=>{
+  test('los datos son enviados con exito de forma correcta', async ()=>{
+    const newObjet = {
+      "title": "bbbb",
+      "author": "miguel",
+      "url": "www/hola.net",
+      "likes": 10,
     } 
-      const response = await api
-      .post('/api/blogs')
-      .send(newObjet)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
+    const response = await api
+    .post('/api/blogs')
+    .send(newObjet)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
 
-      assert.deepStrictEqual(response.body.author, 'miguel')
+    assert.deepStrictEqual(response.body.author, 'miguel')
 
-      const data = await api.get('/api/blogs')
-      assert.strictEqual(data.body.length, initialBLogs.length +1)
-    })
+    const data = await api.get('/api/blogs')
+    assert.strictEqual(data.body.length, initialBLogs.length +1)
+  })
+
+  test('si el campo likes esta vacion se retorna con un 0', async ()=>{
+    const newObject = {
+      "title": "no likes",
+      "author": "pablo",
+      "url": "www/holaaa.net",
+    }
+    const response = await api
+    .post('/api/blogs')
+    .send(newObject)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    assert.deepStrictEqual(response.body.likes, 0)
+  })
 })
 
 after(async () => {
